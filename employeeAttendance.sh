4cf278fc26f6d2a768437888dfb5426f95ebae6f
“rajeshjoshph” <rajeshkannan1999.rk@gmail.com>
2023-01-26 15:34:38 -0800

    Add: Check Employee is Present or Absent

---
+++ employeeAttendance.sh
@@ -0,0 +1,10 @@
+echo "Welcome to Employee Wage Computation Program"
+isPresent=1;
+randomCheck=$((RANDOM%2));
+if [ $isPresent -eq $randomCheck ];
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Abscent"
+fi
+
